56ff295a6c53fba3c41a38dfe90c93d83120068a
guyPepperi <guy.k@pepperi.com>
2022-12-26 09:53:37 +0200

    changed todos endpoint

--- server-side/api.ts
+++ server-side/api.ts
@@ -2,7 +2,8 @@ import { Client, Request } from '@pepperi-addons/debug-server'
 
 export async function test(client: Client, request: Request) {
     return {
-        Hello: 'World'
+        Hello: 'World Gabi'
     }
 };
+
 
